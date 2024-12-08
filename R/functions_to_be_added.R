a6ffd6da505656c37d9520ca3805df3e51ed2d4c
sandyknight <sandyknight90@gmail.com>
2024-12-08 07:34:03 +0000

    discounting & weighting yll function

--- R/functions_to_be_added.R
+++ R/functions_to_be_added.R
@@ -1,68 +1,91 @@
-merge_alcohol_deaths  <- function(){
-ons_alc_deaths <-
-  get_ons_alcohol_specific_death_data()
-
-alc_deaths_tx <-
-get_alcohol_specific_deaths_from_tx(get_non_poisoning_deaths())  |>
-  filter(period_range == "April 2021 to March 2022", death_cause != "Alcohol-specific death" ) |>
-  group_by(sex, age) |>
-  summarise(count = sum(count, na.rm = TRUE), .groups = "drop")
-
-age_df  <<-
-  parse_age_groups(pull(ons_alc_deaths, age_group))
-
-alc_deaths_tx <-
-alc_deaths_tx |>
-  mutate(age_group = assign_age_group(pull(alc_deaths_tx, age), age_df = age_df)) |>
-  group_by(sex, age_group) |>
-  summarise(count = sum(count), .groups = "drop")
-
-alcohol_deaths <-
-bind_rows(alc_deaths_tx, ons_alc_deaths) |>
-  group_by(sex, age_group) |>
-  summarise(count = sum(count), .groups = "drop")
-
-
-alcohol_deaths <-
-alcohol_deaths |>
-  mutate(sex = tolower(sex))
-
-return(alcohol_deaths)
-}
-
-stack_drugs_and_alcohol_deaths  <-
-  function(){
-bind_rows(
-mutate(merge_alcohol_deaths(), substance = "Alcohol"),
-mutate(merge_drug_deaths(), substance  = "Drugs")
-)
+calculate_yll <-
+  function(number.deaths,
+           average.age.death,
+           model.life.expectancy,
+           discount.rate = 0.03,
+           beta.constant = 0.04,
+           modulation.constant = 0,
+           adjustment.constant = 0.1658) {
+    ## abbreviate inputs
+    N <- number.deaths
+    a <- average.age.death
+    L <- model.life.expectancy
+    r <- discount.rate
+    b <- beta.constant
+    K <- modulation.constant
+    CC <- adjustment.constant
+    ## do calculations
+    if (discount.rate == 0) {
+      N * (K * CC * ((exp(-b * a)) / b^2) * ((exp(-b * L)) * (-b * (L + a) - 1) - (-b * a - 1)) + ((1 - K) * L))
+    } else {
+      N * (K * ((CC * exp(r * a)) / (-(r + b)^2)) * ((exp(-(r + b) * (L + a)) * (-(r + b) * (L + a) - 1)) - (exp(-(r + b) * a) * (-(r + b) * a - 1))) + ((1 - K) / r) * ((1 - exp(-r * L))))
+    }
   }
 
-drugs_and_alcohol_deaths <-
-  stack_drugs_and_alcohol_deaths()
 
-life_tables <-
-get_life_tables()
 
+calculate_substance_use_yll <- function(){
+alcohol_deaths <-
+  merge_alcohol_deaths()
+
+age_df <-
+  parse_age_groups(unique(pull(alcohol_deaths, age_group)))
 
 life_tables <-
 life_tables |>
-  mutate(age_group = assign_age_group(pull(life_tables, age), age_df)) |>
+  mutate(age_group = assign_age_group(ages = pull(life_tables, age), age_df = age_df)) |>
   group_by(sex, age_group) |>
-  summarise(ex = mean(ex), .groups = "drop")
+  summarise(mean_age = mean(age), ex = mean(ex), .groups = "drop")
 
-
-
-drugs_and_alcohol_deaths
-life_tables
-
-yll_df <-
-yll_df |>
-  mutate(crude_yll = count * ex) |>
+alcohol_yll <-
+left_join(alcohol_deaths, life_tables) |>
+  mutate(yll = calculate_yll(number.deaths = count, average.age.death = mean_age, model.life.expectancy = ex)) |>
   group_by(age_group, substance) |>
-  summarise(count = sum(count), crude_yll = sum(crude_yll))
+  summarise(count = sum(count), yll = sum(yll), .groups = "drop")
 
 
-yll_df |>
-  ggplot(aes(x = age_group, y = crude_yll)) +
-  geom_col(aes(fill = substance), colour = "black")
+drug_deaths <-
+  merge_drug_deaths()
+
+life_tables <-
+  get_life_tables()
+
+yll <-
+  left_join(drug_deaths, life_tables)
+
+drugs_yll <-
+  yll |>
+  mutate(yll = calculate_yll(number.deaths = count, average.age.death = age, model.life.expectancy = ex))
+
+drugs_yll <-
+drugs_yll |>
+  mutate(age_group = assign_age_group(ages = age, age_df)) |>
+  group_by(age_group, substance) |>
+  summarise(count = sum(count), yll = sum(yll), .groups = "drop")
+
+
+substance_use_yll <-
+  bind_rows(alcohol_yll, drugs_yll)
+
+return(substance_use_yll)
+
+}
+
+substance_use_yll  |>
+  ggplot(aes(x = age_group, y = yll)) +
+  geom_col(aes(fill = substance), colour = "black") +
+  my_theme + my_fill_scale +
+  scale_y_continuous(labels = scales::comma) +
+  labs(
+    fill = NULL,
+    y = "Years of life lost",
+    x = "Age group",
+    title = "Years of life lost due to substance use",
+    caption = "Deaths data from 2021 and 2022",
+    subtitle = glue::glue("Total years of life lost: {scales::comma(sum(substance_use_yll$yll))}")
+  ) +
+  theme(
+    axis.text.x = element_text(angle = 30, vjust = 0.5),
+    legend.direction = "horizontal"
+  )
+
