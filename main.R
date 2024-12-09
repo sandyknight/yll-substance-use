9ac27275c32bb337d7c9e28b76b71315f0da6610
sandyknight <sandyknight90@gmail.com>
2024-12-09 08:02:26 +0000

    add a 'main' script

--- main.R
+++ main.R
@@ -3,6 +3,44 @@
 
 sapply(file.path("R", list.files("R/")), source)
 
+# Produce and save results
+
+crude_yll_estimate <-
+  calculate_crude_yll()
+
+yll_estimate <-
+  calculate_substance_use_yll()
+
+age_group_order <-
+c(
+"<1",
+"01-04",
+"05-09",
+"10-14",
+"15-19",
+"20-24",
+"25-29",
+"30-34",
+"35-39",
+"40-44",
+"45-49",
+"50-54",
+"55-59",
+"60-64",
+"65-69",
+"70-74",
+"75-79",
+"80-84",
+"85-89",
+"90+"
+)
+
+yll_estimate %>%
+
+  select(-count)  |>
+  pivot_wider(names_from = substance, values_from = yll) %>%
+  arrange(age)
+
 # Produce and save plots
 
 p1 <-
