ff93eff932dd194c1a715a235843b811f91d9981
sandy <sandyknight90@gmail.com>
2024-12-19 07:47:49 +0000

    syll formula

---
+++ R/SYLL.R
@@ -0,0 +1,23 @@
+url <- "https://webarchive.nationalarchives.gov.uk/ukgwa/20160106020035mp_/http://www.ons.gov.uk/ons/guide-method/user-guidance/health-and-life-events/age-standardised-mortality-rate-calculation-template-using-the-2013-esp.xls"
+
+esp2013 <- tempfile()
+
+curl::curl_download(url, destfile = esp2013)
+
+esp2013 <-
+  readxl::read_xls(esp2013, sheet = "Methods", range = "B22:C43")
+
+calculate_substance_use_yll()
+
+
+#' Standardised potential years of life lost
+#'
+#' @param d number of deaths
+#' @param a average age-specific lffe expectancy
+#' @param n population in age group
+#' @param w number of people in standard population (ESP 2013) age group
+syll <- function(d, a, n, w) {
+
+  i((w * ((a * d)/n)) / w) * 100e03
+
+}
